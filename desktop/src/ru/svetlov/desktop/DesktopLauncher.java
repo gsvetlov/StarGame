3c12e951b15c904babc460376bee8bf092f386d6
Georg Svetlov <gsvetlov@gmail.com>
2021-05-20 20:48:21 +0300

    Initial project commit

---
+++ desktop/src/ru/svetlov/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.svetlov.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.svetlov.GbStarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GbStarGame(), config);
+	}
+}
