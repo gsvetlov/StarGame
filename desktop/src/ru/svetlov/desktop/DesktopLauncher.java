c3b61d2b7fed2c6f9a7f4b815a7f2c128140ccb1
Georg Svetlov <gsvetlov@gmail.com>
2021-05-20 22:52:52 +0300

    added resizable background texture

--- desktop/src/ru/svetlov/desktop/DesktopLauncher.java
+++ desktop/src/ru/svetlov/desktop/DesktopLauncher.java
@@ -6,7 +6,18 @@ import ru.svetlov.GbStarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		LwjglApplicationConfiguration config = createConfig();
 		new LwjglApplication(new GbStarGame(), config);
+	}
+
+	private static LwjglApplicationConfiguration createConfig() {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "StarGame";
+		config.height = 896;
+		config.width = 448;
+		config.resizable = false;
+		config.x = -1;
+		config.y = 50;
+		return config;
 	}
 }
