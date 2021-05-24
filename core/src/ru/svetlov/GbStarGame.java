e8a8e404919fa80459424068c59d7f745333c9e3
Georg Svetlov <gsvetlov@gmail.com>
2021-05-25 02:46:47 +0300

    InputEventProviderFactory refactored code cleanup

--- core/src/ru/svetlov/GbStarGame.java
+++ core/src/ru/svetlov/GbStarGame.java
@@ -1,10 +1,8 @@
 package ru.svetlov;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
 
 import ru.svetlov.screen.MenuScreen;
-import ru.svetlov.user.controller.BasicUserInputProcessor;
 
 public class GbStarGame extends Game {
 
--- core/src/ru/svetlov/base/InputEventProviderFactory.java
+++ core/src/ru/svetlov/base/InputEventProviderFactory.java
@@ -3,15 +3,14 @@ package ru.svetlov.base;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
-import ru.svetlov.screen.BaseScreen;
 import ru.svetlov.screen.MenuScreen;
 import ru.svetlov.user.controller.BasicUserInputProcessor;
-import ru.svetlov.user.controller.UserInputEventProvider;
+import ru.svetlov.user.controller.events.UserInputEventProvider;
 
 public class InputEventProviderFactory {
     private final static Object lock = new Object();
     private static InputEventProviderFactory factory = null;
-    private final FactoryStorage storage;
+    protected final Dictionary<Class<? extends BaseScreen>, UserInputEventProvider> storage = new Hashtable<>();
 
     public static InputEventProviderFactory getFactory() {
         if (factory != null) return factory;
@@ -21,23 +20,16 @@ public class InputEventProviderFactory {
         return factory;
     }
 
-    private InputEventProviderFactory() {
-        storage = new FactoryStorage();
-    }
+    private InputEventProviderFactory() {}
 
     public UserInputEventProvider getClassUserEventProvider(Object type) {
         if (!(type instanceof BaseScreen)) return null;
-        UserInputEventProvider result = storage.providers.get(type);
-        if (result == null)
-            if (type instanceof MenuScreen){
+        UserInputEventProvider result = storage.get(type);
+        if (result == null){
+            if (type instanceof MenuScreen)
                 result = new BasicUserInputProcessor();
-                storage.providers.put(MenuScreen.class, result);
-            }
+            storage.put(MenuScreen.class, result);
+        }
         return result;
     }
-
-    private static class FactoryStorage {
-        protected final Dictionary<Class<? extends BaseScreen>, UserInputEventProvider> providers = new Hashtable<>();
-    }
-
 }
